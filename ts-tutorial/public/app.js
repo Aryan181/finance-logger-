9d5d06b7d64192da3389da2944272987b3569a3a
Aryan Mahindra <armahin@uw.edu>
2022-05-04 00:01:01 -0700

    updated

---
+++ ts-tutorial/public/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs 
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
---
+++ ts-tutorial/src/app.ts
@@ -0,0 +1,8 @@
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs 
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
